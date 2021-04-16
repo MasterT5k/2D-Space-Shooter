afa5013e1f59861373ba84521a841512038e57ec
Kyle Powers <kwpowers@gmail.com>
2021-04-16 17:32:35 -0400

    All Base Features Implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,6 @@ public class GameManager : MonoBehaviour
 {
     private bool _isGameOver = false;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true)
